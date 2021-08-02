72476d00846b63c188a6b69c476d607917d9cc45
Ankit Singh <ankitsingh4154@gmail.com>
2021-08-02 18:11:53 +0530

    Updated DAO layer,sysout and context.xml for DBconnection pool

--- src/main/java/com/crypto/dao/BookmarkDao.java
+++ src/main/java/com/crypto/dao/BookmarkDao.java
@@ -1,17 +1,23 @@
 package com.crypto.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import com.crypto.DataStore;
 import com.crypto.LocalConnection;
+import com.crypto.constants.BookGenre;
 import com.crypto.entities.Book;
 import com.crypto.entities.Bookmark;
 import com.crypto.entities.Movie;
 import com.crypto.entities.UserBookmark;
 import com.crypto.entities.WebLink;
+import com.crypto.services.BookmarkService;
 
 public class BookmarkDao {
 
@@ -160,5 +166,70 @@ public class BookmarkDao {
 		String query = "delete from Users_Book where users_id = " + userBookmark.getUser().getId() 
 				+ " and book_id = " + userBookmark.getBookmark().getId();
 		System.out.println("Rows deleted : " + stmt.executeUpdate(query) + " from Users_Book table");
+	}
+
+	public Collection<Bookmark> getBooks(boolean isBookmarked, long userId) {
+		Collection<Bookmark> result = new ArrayList<>();
+		try {
+			if (stmt == null) {
+				System.out.println("Null statement reference");
+			}
+
+			String query = "";
+			if(!isBookmarked) {
+				query = "select b.id, b.title, b.image_url, b.publication_year, p.name, "
+						+ "STUFF((SELECT ', ' + a.name FROM Author a join Book_Author ba "
+						+ "		   on a.id = ba.author_id and ba.book_id=b.id "
+						+ "        FOR XML PATH('')), 1, 2, '') as authors, "
+						+ "b.book_genre_id, b.amazon_rating, b.created_date from Book b, Author a, Publisher p, "
+						+ "Book_Author ba where b.publisher_id=p.id and b.id = ba.book_id and ba.author_id = a.id "
+						+ "and b.id NOT IN (select ub.book_id from Users u, Users_Book ub where u.id = "
+						+ userId + " and u.id = ub.users_id) "
+						+ "group by b.id, b.title, b.image_url, b.publication_year, p.name, b.book_genre_id, b.amazon_rating, b.created_date";
+
+			} else {
+				query = "select b.id, b.title, b.image_url, b.publication_year, p.name, "
+						+ "STUFF((SELECT ', ' + a.name FROM Author a join Book_Author ba "
+						+ "		   on a.id = ba.author_id and ba.book_id=b.id "
+						+ "        FOR XML PATH('')), 1, 2, '') as authors, "
+						+ "b.book_genre_id, b.amazon_rating, b.created_date from Book b, Author a, Publisher p, "
+						+ "Book_Author ba where b.publisher_id=p.id and b.id = ba.book_id and ba.author_id = a.id "
+						+ "and b.id IN (select ub.book_id from Users u, Users_Book ub where u.id = "
+						+ userId + " and u.id = ub.users_id) "
+						+ "group by b.id, b.title, b.image_url, b.publication_year, p.name, b.book_genre_id, b.amazon_rating, b.created_date";
+
+			}
+			
+			ResultSet rs = stmt.executeQuery(query);
+			
+			while(rs.next()) {
+
+				long id = rs.getLong("id");
+				String title = rs.getString("title");
+				String imageUrl = rs.getString("image_url");
+				int publicationYear = rs.getInt("publication_year");
+				String publisher = rs.getString("name");
+				String[] authors = rs.getString("authors").split(",");
+				int genreId = rs.getInt("book_genre_id");
+				BookGenre genre = BookGenre.values()[genreId];
+				double amazonRating = rs.getDouble("amazon_rating");
+				
+
+				//Date createdDate = rs.getDate("created_date");
+				/*System.out.println("createdDate: " + createdDate);
+				*/
+				/*System.out.println("id: " + id + ", title: " + title + ", publication year: " + publicationYear
+						+ ", publisher: " + publisher + ", authors: " + String.join(", ", authors) + ", genre: " + genre
+						+ ", amazonRating: " + amazonRating);*/
+				
+				Bookmark bookmark = BookmarkService.getInstance().createBook(id, title, imageUrl, publicationYear, publisher,
+						authors, genre, amazonRating);
+				result.add(bookmark);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
